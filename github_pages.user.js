4bea3c84558cdee12654c72c26546945b7514cdb
Chris Jones <cmj2310@email.vccs.edu>
2024-09-24 12:21:00 -0400

    oops, do I need to update the version number to get Tampermonkey to pull in the revisions?

--- github_pages.user.js
+++ github_pages.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub Pages and Repo Links
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Adds links between GitHub Pages and their corresponding GitHub repositories, and vice versa.
 // @author       You
 // @match        https://*.github.io/*
